c048c899a60d4f059395a3e6c2dbee6b5b2c965a
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-08-17 22:58:47 -0600

    Modified so that script must be run from project root directory

--- setup/download_ncbi.bat
+++ setup/download_ncbi.bat
@@ -1,6 +1,4 @@
 @echo off
-call cd ..\
 call curl https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.12.0/ncbi-blast-2.12.0+-x64-win64.tar.gz > ncbi_blast.tar.gz 
 call tar -xf ncbi_blast.tar.gz 
 call del ncbi_blast.tar.gz 
-call cd setup\
