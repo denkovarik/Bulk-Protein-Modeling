15501ddf6a31033d9ee698316f91829a58dc6b10
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-09-02 21:06:20 -0600

    Modified function exec_commands to take in incremental bar message as parameter

--- protein_modeling.py
+++ protein_modeling.py
@@ -8,32 +8,45 @@ from Bio.PDB import *
 from modeller import *
 from subprocess import Popen, list2cmdline
 from utils import *
+from progress.bar import IncrementalBar
     
-
+# Mark the Start Time
+start_time = time.time()
+# Define filepaths to working and results directory for the PSI BLAST algorithm
 blast_rslt_dir = 'blast_rslts\\'
 blast_working_dir = 'temp_blast\\'
-commands = []
+
+######################### Run the PSI BLAST Algorithm #########################
 # Create and init PSI_BLAST object
 psiblast = PSI_BLAST("ncbi-blast-2.12.0+/bin/psiblast.exe", sys.argv)
 # Check that the required commmand line arguements were passed in
 check_cmd_args(psiblast.args)
 # Compile command line arguments
 queries = PSI_BLAST.parse_fasta(psiblast.args['script_args']['-query_parallel'])
-
+# Compile the commands line commands to run the PSI algorithm 
 commands = psiblast.compile_cmd(psiblast.args, blast_rslt_dir, blast_working_dir)
-
-start_time = time.time()
+#Execute the commands
 psiblast.exec_commands(commands)
-
+# Remove the Working Directory for the PSI BLAST Algorithm
 if os.path.isdir(blast_working_dir):
     shutil.rmtree(blast_working_dir)
+###############################################################################
     
-print("")
+print("\n")
+
 pdb_dir = "pdb\\"
 pdbdownload = PDBList()
 
 tasks = []
 align_files = []
+# Count the number of queries to perform protein modeling on
+num_queries = 0
+for file in os.listdir(blast_rslt_dir):
+    if file.split(".")[0] in queries:
+        num_queries += 1
+        
+bar = IncrementalBar('| Collecting Files for Alignment...\t', max = num_queries)
+
 for file in os.listdir(blast_rslt_dir):
     if file.split(".")[0] in queries:
         seq = queries[file.split(".")[0]]
@@ -41,34 +54,34 @@ for file in os.listdir(blast_rslt_dir):
         with open(blast_rslt_dir + file) as f:
             content = f.read()
         path = blast_rslt_dir + file
-        try:
-            acc = None
-            for protein in iter(BLAST_Rslts_Itr(content, path)):
-                acc = protein['accession'][:protein['accession'].find("_")]
+
+        acc = None
+        for protein in iter(BLAST_Rslts_Itr(content, path)):
+            acc = protein['accession'][:protein['accession'].find("_")]
+            break
+        if acc is not None:
+            #downloaded_path = pdbdownload.retrieve_pdb_file(acc, file_format="pdb", pdir=pdb_dir + acc, overwrite=True)
+            for file in os.listdir(pdb_dir + acc):
+                src = pdb_dir + acc + "\\" + file
+                dest = acc + ".pdb"
+                if not os.path.isfile(dest):
+                    shutil.copy(src , dest)
+                os.remove(src)
                 break
-            if acc is not None:
-                downloaded_path = pdbdownload.retrieve_pdb_file(acc, file_format="pdb", pdir=pdb_dir + acc, overwrite=True)
-                for file in os.listdir(pdb_dir + acc):
-                    src = pdb_dir + acc + "\\" + file
-                    dest = acc + ".pdb"
-                    if not os.path.isfile(dest):
-                        shutil.copy(src , dest)
-                    os.remove(src)
-                    break
                 
-                bsali = ">P1;Bs\nsequence:Bs:::::::0.00: 0.00\n" + seq + "*"
-                f = open("bs.ali", "w")
-                f.write(bsali)
-                f.close()
-                template_path = acc + ".pdb"
-                task = ['py', 'align2d.py', acc, template_path]
-                tasks += [task]
-                ali_file = 'align\Bs-' + acc + '.ali'
-                pap_file = 'align\Bs-' + acc + '.pap'
-                align_files += (ali_file, pap_file)
-                #Popen(task)
-                #os.remove(dest)
-        except:
-            print("An exception occured")
-exec_commands(tasks)
-print("---%s seconds ---" % (time.time() - start_time))
+            bsali = ">P1;Bs\nsequence:Bs:::::::0.00: 0.00\n" + seq + "*"
+            ali_filename = "bs_" + acc + ".ali"
+            f = open(ali_filename, "w")
+            f.write(bsali)
+            f.close()
+            template_path = acc + ".pdb"
+            task = ['py', 'align2d.py', acc, template_path, ali_filename]
+            tasks += [task]
+        bar.next()
+
+print("\n")
+exec_commands(tasks, '| Aligning Proteins...\t')
+
+print("\n")
+
+print("---Runtime: %s seconds ---" % (time.time() - start_time))
