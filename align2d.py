1d280f196ace104f3025818caed6bdadeee9c8b3
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-09-03 00:28:04 -0600

    Fixed bug where the wrong filepath was being used for the atom_files

--- align2d.py
+++ align2d.py
@@ -11,11 +11,11 @@ env = environ()
 aln = alignment(env)
 try:
     mdl = model(env, file=pdbid, model_segment=('FIRST:A','LAST:A'))
-    aln.append_model(mdl, align_codes= pdbid, atom_files=path)
+    aln.append_model(mdl, align_codes= pdbid + "A", atom_files=atom_files_path)
     aln.append(file=ali_path, align_codes='Bs')
     aln.align2d()
-    ali_file = 'align\Bs-' + pdbid + '.ali'
-    pap_file = 'align\Bs-' + pdbid + '.pap'
+    ali_file = 'align\Bs-' + pdbid + 'A.ali'
+    pap_file = 'align\Bs-' + pdbid + 'A.pap'
     aln.write(file=ali_file, alignment_format='PIR')
     aln.write(file=pap_file, alignment_format='PAP')
 except:
